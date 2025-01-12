9405ad6ab5e5aadb50a8e40633debbe875360970
Aniketjamunde9146 <aniketjamunde4@gmail.com>
2025-01-12 12:41:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactive functionality here, such as a button to toggle the menu
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to our hotel menu page!");
+  });
+  
